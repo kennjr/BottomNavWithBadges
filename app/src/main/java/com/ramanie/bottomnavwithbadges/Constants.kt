aeab0bb9379e943620292706a199af5468d9b86c
kenedy <kenedy@myhechar.pro>
2022-11-07 23:30:30 +0300

    created a file for storing all the keys that'll be ref.d by diff files/classes

---
+++ app/src/main/java/com/ramanie/bottomnavwithbadges/Constants.kt
@@ -0,0 +1,7 @@
+package com.ramanie.bottomnavwithbadges
+
+enum class Destinations(val destination: String){
+    HOME_FRAGMENT("home_frag"),
+    CHATS_FRAGMENT("chats_frag"),
+    SETTINGS_FRAGMENT("settings_frag")
+}
