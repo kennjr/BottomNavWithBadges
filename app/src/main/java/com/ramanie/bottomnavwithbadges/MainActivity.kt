b6eaf465410acd61f47b881be92feeabf4b46677
kenedy <kenedy@myhechar.pro>
2022-11-08 00:46:23 +0300

    replaced the surface with a scaffold and added the bottombar to it,  then added the navigation

--- app/src/main/java/com/ramanie/bottomnavwithbadges/MainActivity.kt
+++ app/src/main/java/com/ramanie/bottomnavwithbadges/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.ramanie.bottomnavwithbadges
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.ramanie.bottomnavwithbadges.ui.theme.BottomNavWithBadgesTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BottomNavWithBadgesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                // THIS IS A LAYOUT THAT WE USE WHENEVER WE WANNA SHOW SOME PRE-DEFINED MATERIAL VIEWS(eg FAB, BottomNav, etc),
+                // IT'LL POSITION THE VIEW ACCORDING TO THE GUIDELINES, LIKE PLACING THE BOTTOMNAV AT THE BOTTOM
+                Scaffold( bottomBar = { BottomNavBar(
+                    items = bottomNavItems,
+                    navController = navController,
+                    itemClickEvent = { navController.navigate(it.route) }
+                ) } ) {
+                    Navigation(navHostController = navController)
                 }
             }
         }
