46620dac5ed2870ac7b5411769d9a6a21b5acc98
kenedy <kenedy@myhechar.pro>
2022-11-08 00:46:46 +0300

    created the list of navitems

--- app/src/main/java/com/ramanie/bottomnavwithbadges/Constants.kt
+++ app/src/main/java/com/ramanie/bottomnavwithbadges/Constants.kt
@@ -1,7 +1,18 @@
 package com.ramanie.bottomnavwithbadges
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+
 enum class Destinations(val destination: String){
     HOME_FRAGMENT("home_frag"),
     CHATS_FRAGMENT("chats_frag"),
     SETTINGS_FRAGMENT("settings_frag")
 }
+
+val bottomNavItems = listOf(
+    BottomNavItem("Home", Destinations.HOME_FRAGMENT.destination, icon = Icons.Default.Home, badgeCount = 0),
+    BottomNavItem("Chats", Destinations.CHATS_FRAGMENT.destination, icon = Icons.Default.Email, badgeCount = 10),
+    BottomNavItem("Settings", Destinations.SETTINGS_FRAGMENT.destination, icon = Icons.Default.Settings, badgeCount = 2)
+)
